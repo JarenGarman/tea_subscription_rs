d2b8573f4e9fe45d76f250869147380dd4983a85
Jaren Garman <jarengarman@protonmail.com>
2025-05-19 15:08:33 -0400

    feat: Add rocket initialization with db state

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
+use dotenvy::dotenv;
 use migration::{Migrator, MigratorTrait};
+use rocket::routes;
 
-fn main() {
-    let connection = sea_orm::Database::connect(&database_url).await?;
-    Migrator::up(&connection, None).await?;
+#[rocket::main]
+async fn main() -> Result<(), Box<rocket::Error>> {
+    dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let db = sea_orm::Database::connect(database_url)
+        .await
+        .expect("Failed to connect to the database");
+    Migrator::up(&db, None)
+        .await
+        .expect("Failed to run migrations");
+
+    let _rocket = rocket::build().manage(db).mount("/", routes![]);
+
+    Ok(())
 }
